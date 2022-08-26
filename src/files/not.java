1487336c20f71f0cdb6a948d49eb8292c59c31c9
ubeydemustafa <mustafaubeydeezici@gmail.com>
2022-08-26 17:48:55 +0300

    Temel sınıf oluşturuldu.

---
+++ src/files/not.java
@@ -0,0 +1,15 @@
+package files;
+
+import java.util.Date;
+
+public class not {
+    String isim;
+    String tanım;
+    Date teslimTarihi;
+    int kazanc;
+    double ilerleme;
+    String gereksinimSeviyesi;
+    Date harcananZaman;
+
+
+}
